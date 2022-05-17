aaa0500d68bac4e02563a48171a82366f6086870
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-17 13:26:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Inicio do treinamento de NodeJs");
+const paragrafo = "Teste NodeJs retornando uma funcção";
+
+function texto(string) {
+  return string;
+}
+
+console.log(texto(paragrafo));
