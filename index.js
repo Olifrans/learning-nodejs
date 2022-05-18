d2f167b1fc359c3d7cc5d798781eec209fb66024
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-18 17:03:51 -0300

    feat: cli.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const chalk = require("chalk");
-
 const fs = require("fs");
 
-const expreRegularTexto =
-  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)";
-
-//Expressão regular gerado com suport do "regex101"
+//Expressão regular gerada com suporte do "regex101"
 // => https://regex101.com/
 // => https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Regular_Expressions
 // => https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp
@@ -15,60 +11,33 @@ function extraiLinks(expreRegularTexto) {
 
   const resultArray = [];
   let varTemp;
+
   while ((varTemp = regex.exec(expreRegularTexto)) !== null) {
     resultArray.push({ [varTemp[1]]: varTemp[2] });
   }
-  //const linksExtraidos = expreRegularTexto.match(regex);
-  //const linksExtraidos = regex.exec(expreRegularTexto);
-
-  //console.log(linksExtraidos);
   return resultArray;
 }
-extraiLinks(expreRegularTexto);
 
+//Tratamento de erros
 function trataErro(erro) {
   throw new Error(
     chalk.red(erro.code, "Errrrrrrrooooooo!!!!!!!!!!!!!!! Juninho")
   );
 }
 
-// //Função Assicrona usando - Async e Await
-// async function pegaArquivo(caminhoDoArquivo) {
-//   try {
-//     const encoding = "utf-8";
-//     const resultTexto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-//     console.log(chalk.green(resultTexto));
-//   } catch (erro) {
-//     trataErro(erro);
-//   } finally {
-//     console.log(chalk.yellow("operação concluída"));
-//   }
-// }
-
-////Função Assicrona usando - Then
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = "utf-8";
-//   fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((dataTexto) => chalk.green(console.log(dataTexto)))
-//     .catch((erro) => trataErro(erro));
-// }
-
-////Função Sicrona
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = "utf-8";
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, dataTexto) => {
-//     if (erro) {
-//       trataErro(erro);
-//     }
-//     console.log(chalk.green(dataTexto));
-//   });
-// }
+//Função Assicrona usando - Async e Await
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = "utf-8";
+    const resultTexto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(extraiLinks(resultTexto));
+  } catch (erro) {
+    trataErro(erro);
+  } finally {
+    console.log(chalk.yellow("operação concluída"));
+  }
+}
 
 //pegaArquivo("./arquivos/Readme.md");
 
-// [
-//   {
-//     [nomeLink]: url,
-//   },
-// ];
+module.exports = pegaArquivo;
