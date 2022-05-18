1d544880c053cf995ab1c317f8e59df01a864985
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-18 13:47:04 -0300

    feat: Função  Async e Await

--- index.js
+++ index.js
@@ -4,22 +4,31 @@ const fs = require("fs");
 
 function trataErro(erro) {
   throw new Error(
-    chalk.red(
-      erro.code,
-      "Errrrrrrrooooooo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Juninho"
-    )
+    chalk.red(erro.code, "Errrrrrrrooooooo!!!!!!!!!!!!!!! Juninho")
   );
 }
 
-//Função assicrona
-function pegaArquivo(caminhoDoArquivo) {
-  const encoding = "utf-8";
-  fs.promises
-    .readFile(caminhoDoArquivo, encoding)
-    .then((dataTexto) => console.log(dataTexto))
-    .catch((erro) => trataErro(erro));
+//Função Assicrona usando - Async e Await
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = "utf-8";
+    const resultTexto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(chalk.green(resultTexto));
+  } catch (erro) {
+    trataErro(erro);
+  }
 }
 
+////Função Assicrona usando - Then
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = "utf-8";
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((dataTexto) => chalk.green(console.log(dataTexto)))
+//     .catch((erro) => trataErro(erro));
+// }
+
+////Função Sicrona
 // function pegaArquivo(caminhoDoArquivo) {
 //   const encoding = "utf-8";
 //   fs.readFile(caminhoDoArquivo, encoding, (erro, dataTexto) => {
